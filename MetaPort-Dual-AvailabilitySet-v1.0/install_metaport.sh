29463758ca639983224c2bef634ebef8fbaf6e26
William Guilherme <wguilherme@securitygeek.io>
2020-10-17 21:39:14 -0700

    updates

---
+++ MetaPort-Dual-AvailabilitySet-v1.0/install_metaport.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo mkdir build
+sudo wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
+sudo chmod +x build/build_mp.sh
+sudo ./build/build_mp.sh
+sleep 1m
+metaTokenCode=$1
+su mpadmin -c "metaport onboard $metaTokenCode"
