5d42c90e95e5148f8cf86e0a408a1bb0d9c9f134
William Guilherme <wguilherme@securitygeek.io>
2020-10-21 01:33:14 -0700

    First Commit

--- MetaPort-Dual-AvailabilitySet-v1.0/install_metaport.sh
+++ MetaPort-Dual-AvailabilitySet-v1.0/install_metaport.sh
@@ -3,7 +3,7 @@ sudo mkdir build
 sudo wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
 sudo chmod +x build/build_mp.sh
 sudo ./build/build_mp.sh
-sleep 1m
+sleep 20
 MP1TokenCode=$1
 MP2TokenCode=$1
 su mpadmin -c "metaport onboard $MP1TokenCode"
