f1466b8e168dc8237f79bf62f80e2dfe9dc27b98
William Guilherme <wguilherme@securitygeek.io>
2020-10-22 17:44:47 -0700

    update

--- MetaPort-Dual-AvailabilitySet-v3.0/install_metaport.sh
+++ MetaPort-Dual-AvailabilitySet-v3.0/install_metaport.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Creating build directory and downloading MetaPort LXD packages
+echo "Creating build directory and downloading MetaPort LXD packages"
 sudo mkdir build
 sudo wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
 sudo chmod +x build/build_mp.sh
 
-# Initiating MetaPort package installation
+echo "Initiating MetaPort package installation"
 sudo ./build/build_mp.sh
 sleep 15
 
-# Token Code provided by the Customer during the Wizard configuration
+echo "Token Code provided by the Customer during the Wizard configuration"
 MP1TokenCode=$1
 MP2TokenCode=$2
 
-# Initiating Onboarding Process for MetaPort-1
+echo "Initiating Onboarding Process for MetaPort-1"
 su mpadmin -c "metaport onboard $MP1TokenCode"
 
-# Initiating Onboarding Process for MetaPort-2
+echo "Initiating Onboarding Process for MetaPort-2"
 su mpadmin -c "metaport onboard $MP2TokenCode"
