c69d1e60cb48f7459350067d0ce886c0bfab0e56
William Guilherme <wguilherme@securitygeek.io>
2020-10-25 23:38:00 -0700

    update

--- Latest/MetaPort-Standalone-NATGW-v2.0/install_metaport.sh
+++ Latest/MetaPort-Standalone-NATGW-v2.0/install_metaport.sh
@@ -3,5 +3,6 @@ sudo mkdir build
 sudo wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
 sudo chmod +x build/build_mp.sh
 sudo ./build/build_mp.sh
+sleep 30
 metaTokenCode=$1
-su mpadmin -c "metaport onboard ${metaTokenCode}"
+su mpadmin -c "metaport onboard $metaTokenCode"
