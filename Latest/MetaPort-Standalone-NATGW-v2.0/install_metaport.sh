14b0f8f0c0f4989dc56152d2d4fe4d965eaa1c05
William Guilherme <wguilherme@securitygeek.io>
2020-10-26 12:01:00 -0700

    update

--- Latest/MetaPort-Standalone-NATGW-v2.0/install_metaport.sh
+++ Latest/MetaPort-Standalone-NATGW-v2.0/install_metaport.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Creating build directory and downloading MetaPort LXD packages
+sudo apt-get -qq -y update
+sudo apt-get -q -y install software-properties-common
+sudo apt-get -q -y install python3-pip
+sudo apt-get -q -y python3-distutils
+sudo apt-get -q -y build-essential
 sudo mkdir build
 sudo wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
 sudo chmod +x build/build_mp.sh
@@ -9,7 +14,7 @@ sudo ./build/build_mp.sh
 sleep 1m
 
 # Token Code provided by the Customer during the Wizard configuration
-wait metaTokenCode=$1
+metaTokenCode=$1
 
 # Initiating Onboarding Process
 su mpadmin -c "metaport onboard $metaTokenCode"
