57123e44b3c96347822e004b5ef5c5340e70ce31
William Guilherme <wguilherme@securitygeek.io>
2020-10-26 13:07:10 -0700

    update

--- Latest/MetaPort-Standalone-v2.0/install_metaport.sh
+++ Latest/MetaPort-Standalone-v2.0/install_metaport.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 # Creating build directory and downloading MetaPort LXD packages
-mkdir build
-wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
-chmod +x build/build_mp.sh
+sudo apt-get -qq -y update
+sudo apt-get -q -y install software-properties-common
+sudo apt-get -q -y python3-distutils
+sudo apt-get -q -y install python3-pip
+sudo apt-get -q -y build-essential
+sudo mkdir build
+sudo wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
+sudo chmod +x build/build_mp.sh
 
-./build/build_mp.sh
-sleep 1m
+sudo ./build/build_mp.sh
+sleep 15
 
 # Token Code provided by the Customer during the Wizard configuration
 metaTokenCode=$1
 
 # Initiating Onboarding Process
-metaport onboard $metaTokenCode
+su mpadmin -c "metaport onboard $metaTokenCode"
