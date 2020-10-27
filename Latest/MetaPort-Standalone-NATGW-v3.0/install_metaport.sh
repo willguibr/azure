3ffc029fb0422dc9029fab5881677767ad9ebca6
William Guilherme <wguilherme@securitygeek.io>
2020-10-27 11:09:22 -0700

    update

--- Latest/MetaPort-Standalone-NATGW-v3.0/install_metaport.sh
+++ Latest/MetaPort-Standalone-NATGW-v3.0/install_metaport.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 # Creating build directory and downloading MetaPort LXD packages
+# export DEBIAN_FRONTEND=noninteractive
 # sudo apt-get -qq -y update
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -q -y install software-properties-common
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -q -y install python3-pip
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -y python3-wheel
-export DEBIAN_FRONTEND=noninteractive
-sudo pip3 install wheel
+# export DEBIAN_FRONTEND=noninteractive
+# sudo apt-get -q -y install software-properties-common
+# export DEBIAN_FRONTEND=noninteractive
+# sudo apt-get -q -y install python3-pip
+# export DEBIAN_FRONTEND=noninteractive
+# sudo apt-get install -y python3-wheel
+# export DEBIAN_FRONTEND=noninteractive
+#  pip3 install wheel
 sudo mkdir build
 sudo wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
 sudo chmod +x build/build_mp.sh
 
 # Starting MetaPort Software Installation
-export DEBIAN_FRONTEND=noninteractive
+# export DEBIAN_FRONTEND=noninteractive
 sudo ./build/build_mp.sh
 sleep 10
 
