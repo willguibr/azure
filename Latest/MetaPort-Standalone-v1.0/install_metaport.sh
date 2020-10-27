ab5fb8dec50eb88e4ec80d32f34599ac4d6aadef
William Guilherme <wguilherme@securitygeek.io>
2020-10-27 14:33:30 -0700

    updated install_metaport script

--- Latest/MetaPort-Standalone-v1.0/install_metaport.sh
+++ Latest/MetaPort-Standalone-v1.0/install_metaport.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 # Creating build directory and downloading MetaPort LXD packages
+sudo ex +"%s@DPkg@//DPkg" -cwq /etc/apt/apt.conf.d/70debconf
+sudo dpkg-reconfigure debconf -f noninteractive -p critical
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -qq -y update
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -q -y install software-properties-common
-# sudo apt-get -q -y python3-distutils
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -q -y install python3-pip
-# sudo apt-get -q -y build-essential
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get install -y python3-wheel
+sudo rm -rf /var/lib/apt/lists/*
+sudo apt-get -qq -y update
 sudo mkdir build
 sudo wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
 sudo chmod +x build/build_mp.sh
 
+# Starting MetaPort Software Installation
+export DEBIAN_FRONTEND=noninteractive
 sudo ./build/build_mp.sh
 sleep 10
 
