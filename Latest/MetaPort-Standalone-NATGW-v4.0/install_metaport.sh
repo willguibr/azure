d353f393814b202cda0bdc79e0ed929e8d979c0a
William Guilherme <wguilherme@securitygeek.io>
2020-10-27 08:36:49 -0700

    deleted v4.0 directory

--- Latest/MetaPort-Standalone-NATGW-v4.0/install_metaport.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Token Code provided by the Customer during the Wizard configuration
-metaTokenCode=$1
-
-# Initiating Onboarding Process
-su mpadmin -c "metaport onboard $metaTokenCode"
