b7b7ea3b61dd257bfd8be4335e63b89901cc787d
William Guilherme <wguilherme@securitygeek.io>
2020-10-28 00:42:58 -0700

    update

--- Latest/MetaPort-Standalone-NATGW-v4.0/install_metaport.sh
+++ Latest/MetaPort-Standalone-NATGW-v4.0/install_metaport.sh
@@ -7,6 +7,4 @@ sleep 10
 
 # Token Code provided by the Customer during the Wizard configuration
 metaTokenCode=$1
-
-# Initiating Onboarding Process
 su mpadmin -c "metaport onboard $metaTokenCode"
