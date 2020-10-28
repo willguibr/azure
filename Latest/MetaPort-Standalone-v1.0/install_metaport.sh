a632e7b68aa10d9c462a3b1935ce1261af38d4f7
William Guilherme <wguilherme@securitygeek.io>
2020-10-28 09:10:56 -0700

    update

--- Latest/MetaPort-Standalone-v1.0/install_metaport.sh
+++ Latest/MetaPort-Standalone-v1.0/install_metaport.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Creating build directory and downloading MetaPort LXD packages
-sudo ex +"%s@DPkg@//DPkg" -cwq /etc/apt/apt.conf.d/70debconf
-sudo dpkg-reconfigure debconf -f noninteractive -p critical
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -qq -y update
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -q -y install software-properties-common
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get -q -y install python3-pip
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -y python3-wheel
-sudo rm -rf /var/lib/apt/lists/*
-sudo apt-get -qq -y update
-sudo mkdir build
-sudo wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
-sudo chmod +x build/build_mp.sh
+# sudo ex +"%s@DPkg@//DPkg" -cwq /etc/apt/apt.conf.d/70debconf
+# sudo dpkg-reconfigure debconf -f noninteractive -p critical
+# export DEBIAN_FRONTEND=noninteractive
+# sudo apt-get -qq -y update
+# export DEBIAN_FRONTEND=noninteractive
+# sudo apt-get -q -y install software-properties-common
+# export DEBIAN_FRONTEND=noninteractive
+# sudo apt-get -q -y install python3-pip
+# export DEBIAN_FRONTEND=noninteractive
+# sudo apt-get install -y python3-wheel
+# sudo rm -rf /var/lib/apt/lists/*
+# sudo apt-get -qq -y update
+# sudo mkdir build
+# sudo wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
+# sudo chmod +x build/build_mp.sh
 
 # Starting MetaPort Software Installation
-export DEBIAN_FRONTEND=noninteractive
-sudo ./build/build_mp.sh
-sleep 10
+# export DEBIAN_FRONTEND=noninteractive
+# sudo ./build/build_mp.sh
+#  sleep 10
 
 # Token Code provided by the Customer during the Wizard configuration
 metaTokenCode=$1
