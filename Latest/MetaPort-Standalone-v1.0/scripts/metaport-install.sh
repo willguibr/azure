85fbcf592604f107e1c1ec801bd6acfbc8712f0f
William Guilherme <wguilherme@securitygeek.io>
2020-10-28 16:01:19 -0700

    update

--- Latest/MetaPort-Standalone-v1.0/scripts/metaport-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sleep 30
-mkdir build
-wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
-chmod +x build/build_mp.sh
-./build/build_mp.sh
-sleep 15
