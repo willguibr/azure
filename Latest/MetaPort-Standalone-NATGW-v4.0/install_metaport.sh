d7c70321f066e29bf78225237f350aa6ba0cf468
William Guilherme <wguilherme@securitygeek.io>
2020-10-28 00:42:44 -0700

    update

--- Latest/MetaPort-Standalone-NATGW-v4.0/install_metaport.sh
+++ Latest/MetaPort-Standalone-NATGW-v4.0/install_metaport.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
-# Downloading MetaPort Software
 mkdir build
 wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
 chmod +x build/build_mp.sh
-
-# Starting MetaPort Software Installation
 ./build/build_mp.sh
 sleep 10
 
