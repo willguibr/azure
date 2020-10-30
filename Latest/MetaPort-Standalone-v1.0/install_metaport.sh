5a42ff8d7a7fc9a6965302262f5553e4388b637c
William Guilherme <wguilherme@securitygeek.io>
2020-10-30 01:06:58 -0700

    update

--- Latest/MetaPort-Standalone-v1.0/install_metaport.sh
+++ Latest/MetaPort-Standalone-v1.0/install_metaport.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-sleep 15
-mkdir build
-wget -q -Obuild/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
-chmod +x build/build_mp.sh
-./build/build_mp.sh
-sleep 15
 
 # Token Code provided by the Customer during the Wizard configuration
 metaTokenCode=$1
