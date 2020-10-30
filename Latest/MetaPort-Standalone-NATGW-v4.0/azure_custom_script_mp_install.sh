7acc101a44831eed155eb9a13fcc0a61996c392f
William Guilherme <wguilherme@securitygeek.io>
2020-10-30 01:55:35 -0700

    update

--- Latest/MetaPort-Standalone-NATGW-v4.0/azure_custom_script_mp_install.sh
+++ Latest/MetaPort-Standalone-NATGW-v4.0/azure_custom_script_mp_install.sh
@@ -1,15 +1,16 @@
 #!/bin/sh -e
 sleep 15
 
-# download
+# Download the latest MetaPort package(s)
 wget -q -O/tmp/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
 chmod +x /tmp/build_mp.sh
 
-# install (as ubuntu)
+# Initiate MetaPort Install (as ubuntu)
 sudo -u ubuntu -i sh -c 'cd /tmp && /tmp/build_mp.sh'
 sleep 15
 
+# Token Code provided by the Customer during the Wizard configuration
 metaTokenCode=$1
 
-# onboard (as mpadmin)
+# Starting Onboard process (as mpadmin)
 sudo -u mpadmin -i metaport onboard $metaTokenCode > metaport_onboard.txt
