b5ddc6d43a37b6513eb1460a624c6b739fd2ecf1
William Guilherme <wguilherme@securitygeek.io>
2020-10-30 01:35:05 -0700

    update

--- Latest/MetaPort-Standalone-v1.0/install_metaport.sh
+++ Latest/MetaPort-Standalone-v1.0/install_metaport.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/sh -e
+sleep 15
 
-# Token Code provided by the Customer during the Wizard configuration
+# download
+wget -q -O/tmp/build_mp.sh https://s3.amazonaws.com/public.nsof.io/lxd/metaport-install.sh
+chmod +x /tmp/build_mp.sh
+
+# install (as ubuntu)
+sudo -u ubuntu -i sh -c 'cd /tmp && /tmp/build_mp.sh'
+sleep 15
+
 metaTokenCode=$1
 
-# Initiating Onboarding Process
+# onboard (as mpadmin)
 sudo -u mpadmin -i metaport onboard $metaTokenCode > metaport_onboard.txt
