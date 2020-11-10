6527a1abcc23c77ac663ffc6a661681d1656277f
William Guilherme <wguilherme@securitygeek.io>
2020-11-10 12:35:55 -0800

    deleted sandbox directory

--- Sandbox/MetaPort-Standalone-NATGW-v4.0/install_metaport.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Token Code provided by the Customer during the Wizard configuration
-metaTokenCode=$1
-
-# Initiating Onboarding Process
-sudo -u mpadmin -i metaport onboard $metaTokenCode > metaport_onboard.txt
